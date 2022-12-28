06a823868378f5e1d8722c8f2dc8f09cf0431528
ryan <rdewalt@gmail.com>
2022-12-28 09:12:05 -0800

    debugging

--- website/rwdfoo.php
+++ website/rwdfoo.php
@@ -25,7 +25,7 @@ print "<pre>";
 print_r($_SESSION);
 print  "<hr>";
 $bd=date_create($_SESSION["B"]);
-$now=date_create(gmdate(DATE_ATOM));
+$now=date_create(date("m-d-Y"));
 $diff=date_diff($bd,$now);
 print "-- $diff";
 /*
