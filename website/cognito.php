2c6407bef27c5f8345ee42669c5a6d0f8c745e19
ryan <rdewalt@gmail.com>
2022-12-27 22:31:27 -0800

    U1

--- website/cognito.php
+++ website/cognito.php
@@ -54,17 +54,13 @@ $C_UN=$response["username"];
 $C_Email=$response["email"];
 $folder= "a/". substr($C_UID,0,2) . "/" . $C_UID . "/";
 
-// Set into cookies that expire when we're told they can.
-
 $_SESSION["I"]=$id_token;
 $_SESSION["A"]=$access_token;
 $_SESSION["R"]=$refresh_token;
 $_SESSION["U"]=$C_UID;
-$_SESSION["folder"]= $folder;
+$_SESSION["folder"]= $folder; 
 $_SESSION['loggedin'] = true;
 
-
-// OKAY, here we do the new user check and setup.
 $dbh=getDBH();
 $sql = "select * from users where cog_id = :CID";
 $sth = $dbh->prepare($sql);
@@ -117,8 +113,5 @@ if ( count($foo) )  {
     }
 }
 
-    setcookie("U",$C_UID, time()+$expires_in,"/","yna.solfire.com",1,1);
-
     header ("Location: /");
-
 ?>
--- website/up.php
+++ website/up.php
@@ -14,17 +14,8 @@ if (!($Ext=="png" or $Ext=="gif" or $Ext=="jpg" or $Ext=="jpeg")) {
 		header ("Location: /upload.php?e=BadThingImage");
 		exit();
 }
-if ($_FILES['Thumbnail']['error']==0) {
-	$Thumb_name=$_FILES['Thumbnail']['name'];
-	$ThumbExtension=explode(".",$Thumb_name);
-	$ThumbExt=strtolower($ThumbExtension[count($ThumbExtension)-1]);
-	if (!($ThumbExt=="png" or $ThumbExt=="gif" or $ThumbExt=="jpg" or $ThumbExt=="jpeg")) {
-			header ("Location: /upload.php?e=BadThingThumbnail");
-			exit();
-	}
-}
 
-if ($_FILES['Thumbnail']['error']==0) { $ThumbnailYes=true; } else { $ThumbnailYes = false;}
+if ($_FILES['Thumbnail']['error']==0) { $ThumbnailYes=true; } else 	{ $ThumbnailYes = false;}
 
     $Title = filter_input(INPUT_POST, 'ImageTitle', FILTER_SANITIZE_STRING);
     $Keywords = filter_input(INPUT_POST, 'ImageKeywords', FILTER_SANITIZE_STRING);
@@ -32,14 +23,15 @@ if ($_FILES['Thumbnail']['error']==0) { $ThumbnailYes=true; } else { $ThumbnailY
 	$Category=$_POST['category']; $Folder=$_POST['Folder'];
 	$NSFW=$_POST['Adult'];
 	if (isset($_POST['skip'])) {$SkipRecent=$_POST['skip']; }
+
 	$OriginalFilename=$_FILES['Image']['name'];
-	$Shard=substr(md5($_FILES['Image']['tmp_name'].$_SESSION['login_string']),2,2); // Just needs to be random.
-	$GDir="/var/www/html/A/".$Shard;
-	@mkdir ($GDir,0755,true);
+	$Shard=$_SESSION["folder"]
 	$UserID=$_SESSION['user_id'];
 	$dbh=getDBH();
+	
 	list($width, $height, $type, $attr) = getimagesize($_FILES['Image']['tmp_name']);
-	$sql="insert into images (ImageID,UserID,Title,FolderID,Category,NSFW,OriginalFilename,State,Shard,height,width,Keywords,Description,UploadDate) values (NULL,:UserID,:Title,:FolderID,:Category,:NSFW,:OriginalFilename,'N',:Shard,:height,:width,:Keywords,:Description,now())";
+	$sql="insert into images (ImageID,UserID,Title,FolderID,Category,NSFW,OriginalFilename,State,Shard,height,width,Keywords,Description,UploadDate) values 
+	(NULL,:UserID,:Title,:FolderID,:Category,:NSFW,:OriginalFilename,'N',:Shard,:height,:width,:Keywords,:Description,now())";
 	// "N" for state is "NEW" (not ready)
     if ($stmt = $dbh->prepare($sql)) {
         $stmt->bindParam(':UserID', $UserID);
@@ -58,132 +50,41 @@ if ($_FILES['Thumbnail']['error']==0) { $ThumbnailYes=true; } else { $ThumbnailY
 
     $ImageRecordID=$dbh->lastInsertId();
     $ShortImage=toBase($ImageRecordID);
-    $Filename=$ShortImage.".".$Ext;
-	if ($ThumbnailYes) {
-	    $Thumbnail="T_".$ShortImage.".".$ThumbExt;
-	}
-	else
-	{
-	    $Thumbnail="T_".$ShortImage.".".$Ext;
-	}
+    $Filename=$OriginalFilename;
     $TempFile=$_FILES['Image']['tmp_name'];
-    copy ("$TempFile","$GDir/$Filename");
-    $ThumbFile="$GDir/$Thumbnail";
 
 
-/// Thumbnail Handling Code Here
-if ($ThumbnailYes) { // Has Thumbnail
-    $TempThumb=$_FILES['Thumbnail']['tmp_name'];
-    copy ("$TempThumb","$ThumbFile");
-	}  else
-	{  // Has no thumbnail.
-		if ( $Ext=="png" or $Ext=="gif" or $Ext=="jpg" or $Ext=="jpeg" ) {
-			    copy ("$TempFile","$ThumbFile");
-			    $ThumbExt=$Ext;
-			   }
-	}
-
-if ( $Ext=="png" or $Ext=="gif" or $Ext=="jpg" or $Ext=="jpeg" or $ThumbnailYes ) {
-
-if ($width>450 or $height>150)
-{
-	// Load Image
-		switch ($ThumbExt) {
-			case "png" :
-			$img=imagecreatefrompng($ThumbFile);
-			break;
-			case "gif" :
-			$img=imagecreatefromgif($ThumbFile);
-			break;
-			case "jpeg":
-			case "jpg":
-			$img=imagecreatefromjpeg($ThumbFile);
-			break;
-		}
-//	RESCALE to 150x150;
-		$img3=imagescale($img,450,-1);
-		$img2=imagecrop($img3, ['x' => 0, 'y' => 0, 'width' => 450, 'height' => 200]);
-	// Now Save it back out;
-		switch ($ThumbExt) {
-			case "png" :
-			imagepng($img2, $ThumbFile);
-			break;
-			case "gif" :
-			imagegif($img2, $ThumbFile);
-			break;
-			case "jpeg":
-			case "jpg":
-			imagejpeg($img2, $ThumbFile);
-			break;
-		}
-	}
-	$MediumFile=false;
-	// Okay, if the image is greater than 800 wide, we make a "medium" version intermediary.
-	if ($width>800)  {
-		$MediumFile=true; $MediumFilename="m_$Filename";
-		// Load Image
-			switch ($Ext) {
-				case "png" :
-				$img=imagecreatefrompng("$GDir/$Filename");
-				break;
-				case "gif" :
-				$img=imagecreatefromgif("$GDir/$Filename");
-				break;
-				case "jpeg":
-				case "jpg":
-				$img=imagecreatefromjpeg("$GDir/$Filename");
-				break;
-			}
-//	RESCALE to 800 wide, store with m_$filename.
-
-			$img2=imagescale($img,800,-1);
-		// Now Save it back out;
-			switch ($Ext) {
-				case "png" :
-				imagepng($img2, "$GDir/$MediumFilename");
-				break;
-				case "gif" :
-				imagegif($img2, "$GDir/$MediumFilename");
-				break;
-				case "jpeg":
-				case "jpg":
-				imagejpeg($img2, "$GDir/$MediumFilename");
-				break;
-			}
-
-	}  else
-	// If the original is less than 800 wide, we don't need a "medium" state.
-	{ $MediumFilename=$Filename; }
-}
-
-// And now we need to update the database entry with the right filename things.
-// And set the image "Live"
-		if (isSet($SkipRecent)){
-	        if ($SkipRecent=="Y") {
-	        	$State="S";
-	        } else
-	        {
-	        	$State="L";
-	    	}
-    	} else 	{
-	        	$State="L";
-	    }
+	$s3Client = new S3Client([
+        'profile' => 'default',
+        'region' => 'us-west-2',
+        'version' => '2006-03-01'
+    ]);
+    $bucket= "yna-images"; 
+    $s3Client ->putObject(array(
+        'Bucket' => $bucket,
+        'Key'    => $folder . $ShortImage . "." . $Ext,
+        'SourceFile'   => $TempFile,
+        'ACL'    => 'public-read'
+       ));
 
 
-$sql="update images set ShortID=:ShortID, Filename=:Filename, State=:State, Medium=:Medium, Thumbnail=:Thumbnail where ImageID=:ImageID and UserID=:UserID";
+//	copy ("$TempFile","$GDir/$Filename");
+// 	$ThumbFile="$GDir/$Thumbnail";
+
+
+
+$sql="update images set ShortID=:ShortID, Filename=:Filename, State=:State where ImageID=:ImageID and UserID=:UserID";
     if ($stmt = $dbh->prepare($sql)) {
         $stmt->bindParam(':ImageID', $ImageRecordID);
         $stmt->bindParam(':UserID', $UserID);
         $stmt->bindParam(':ShortID', $ShortImage);
         $stmt->bindParam(':State', $State);
         $stmt->bindParam(':Filename', $Filename);
-        $stmt->bindParam(':Medium', $MediumFilename);
         $stmt->bindParam(':Thumbnail', $Thumbnail);
         $stmt->execute();
     }
 
 // Now, if this was a Category 36 upload (My User Icon)  we set this ID to be our User Icon.
-
 //create table UserIcon (UserID int unsigned, imageid int unsigned, username varchar(30) ) ;
 
     if ($Category==36) {
