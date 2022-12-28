62eb364809c1a0e645d9b37f498b3c427c72cf29
ryan <rdewalt@gmail.com>
2022-12-28 07:26:18 -0800

    debugging login

--- website/rwdfoo.php
+++ website/rwdfoo.php
@@ -1,20 +1,7 @@
 <?php
+require_once "library.inc";
 
-require 'vendor/autoload.php';
-
-use Aws\Iam\IamClient;
-use Aws\S3\S3Client;
-use Aws\Exception\AwsException;
-
-$s3Client = new S3Client([
-    'profile' => 'default',
-    'region' => 'us-west-2',
-    'version' => '2006-03-01'
-]);
-
-$buckets = $s3Client->listBuckets();
-foreach ($buckets['Buckets'] as $bucket) {
-    echo $bucket['Name'] . "\n";
-}
+print "<pre>";
+print_r($_SESSION);
 
 ?>
