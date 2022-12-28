a0c6adbddbc8a636df54a2f8ae451312679c91e9
ryan <rdewalt@gmail.com>
2022-12-28 07:40:28 -0800

    debugging login

--- website/u.php
+++ website/u.php
@@ -56,7 +56,7 @@ global $PUID;
 			}
 			</style>
 			<div class="userpageimage">
-			<a href="i.php?id=<?=$f['ImageID']?>"><img class="frontpageimage" src="https://cdn2.yna.solfire.com/<?=$i['shard']?><?=$i['Filename']?>"></a>
+			<a href="i.php?id=<?=$f['ImageID']?>"><img class="frontpageimage" src="https://cdn2.yna.solfire.com/<?=$f['shard']?><?=$f['Filename']?>"></a>
 			</div>
 		<?
 		}
