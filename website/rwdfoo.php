dfd4aa169c3e491d51632f7a5513594a5a821803
ryan <rdewalt@gmail.com>
2022-12-28 08:24:32 -0800

    debugging login

--- website/rwdfoo.php
+++ website/rwdfoo.php
@@ -5,7 +5,21 @@ require 'vendor/autoload.php';
 use Aws\Iam\IamClient;
 use Aws\S3\S3Client;
 use Aws\Exception\AwsException;
+use Aws\CognitoIdentityProvider\CognitoIdentityProviderClient;
 
+$client = new CognitoIdentityProviderClient([
+    'profile' => 'default',
+    'region' => 'us-west-2',
+    'version' => '2006-03-01'
+]);
+
+$result = $client=>listUserPools([
+    'MaxResults' => 100
+]);
+
+print "<pre>";
+print_r($result);
+/*
 $s3Client = new S3Client([
     'profile' => 'default',
     'region' => 'us-west-2',
@@ -16,5 +30,7 @@ $buckets = $s3Client->listBuckets();
 foreach ($buckets['Buckets'] as $bucket) {
     echo $bucket['Name'] . "\n";
 }
+*/
+
 
 ?>
