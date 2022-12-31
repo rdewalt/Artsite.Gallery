f2bd47bdaed8c17d01227eb380e9703f4ee9791e
ryan <rdewalt@gmail.com>
2022-12-31 22:18:09 +0000

    hourly database running task

--- terraform/lambdas/hourly_runner/hourly.py
+++ terraform/lambdas/hourly_runner/hourly.py
@@ -1,22 +1,29 @@
 import pymysql
   
-def lambda_handler(event, context):
+def mysqlconnect():
+    # To connect MySQL database
     conn = pymysql.connect(
         host='mysql',
         user='yna', 
         password = "86753091024",
         db='yna',
         )
+      
     cur = conn.cursor()
     cur.execute("select count(*),ImageID from yna.image_faves group by ImageID")
     output = cur.fetchall()
     for x,y in output:
         print("{} - {}".format(x,y))
         cur.execute("update images set FaveCount={} where ImageID={}".format(x,y))
-    cur.execute("select count(*), ImageID from yna.image_views group by ImageID")
+
+    cur.execute("select count(*) c, ImageID from yna.image_views group by ImageID)
     output = cur.fetchall()
     for x,y in output:
         print("{} - {}".format(x,y))
-        cur.execute("update images set ViewCount={} where ImageID={}".format(x,y))      
+        cur.execute("update images set FaveCount={} where ImageID={}".format(x,y))      
     conn.close()
-    return "Success"
+
+
+# Driver Code
+if __name__ == "__main__" :
+    mysqlconnect()
